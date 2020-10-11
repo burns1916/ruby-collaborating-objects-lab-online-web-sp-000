fc5ab7c464684e60aba8a4e6e923d68c002997be
Ryan K Burns <ryankburns@gmail.com>
2020-10-11 01:16:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,9 +20,14 @@ class Song
   def self.new_by_filename(filename)
     song = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[0]
-    Song.new(song)
-    song.artist = artist_name
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
 end
