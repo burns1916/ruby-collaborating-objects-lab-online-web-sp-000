363d4991b1f81cc6594089ceede2a8ad613476e7
Ryan K Burns <ryankburns@gmail.com>
2020-10-11 00:40:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+
+  attr_accessor :name, :artist
+
+
+  def initialize(name)
+    @name = name
+  end
+
+
+  def self.new_by_filename(filename)
+    song = self.new(name)
+    song.name = filename.split(" - ")[1]
+    song
+  end
+
+  def artist_name=(name)
+    if self.artist == nil
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
+end
