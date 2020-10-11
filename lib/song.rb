7eddbe4fb74ad6d0e56166905c8ab3caac4ae02f
Ryan K Burns <ryankburns@gmail.com>
2020-10-11 01:04:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,17 @@
 class Song
 
-  attr_accessor :name, :Artist
+  attr_accessor :name, :artist
+  
+  @@all = []
 
   def initialize(name)
     @name = name
   end
+
+  def artist(artist)
+    
+
+  
 
 
 
