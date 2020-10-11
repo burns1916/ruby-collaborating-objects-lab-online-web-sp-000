aa203c857933c09370171d310969da5d3a019edd
Ryan K Burns <ryankburns@gmail.com>
2020-10-11 01:03:40 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,12 @@ class MP3Importer
     @path = filename
   end
 
+  def files
+    
+  end
 
-
+  def import(list_of_filenames)
+    list_of_filenames.each{|filename| Song.new_by_filename(filename)}
+  end
 
 end
